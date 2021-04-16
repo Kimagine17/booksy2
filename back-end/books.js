d6fd4958e7820da7c680297116ef22260e570ae5
Hannah Nelson <hannahnelson@null-50ed3c3c53ca.app.byu.edu>
2021-04-16 00:53:36 -0600

    working on it

---
+++ back-end/books.js
@@ -0,0 +1,135 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const router = express.Router();
+
+// Configure multer so that it will upload to '../front-end/public/images'
+const multer = require('multer')
+const upload = multer({
+  dest: '../front-end/public/images/',
+  limits: {
+    fileSize: 10000000
+  }
+});
+
+const users = require("./users.js");
+const genres = require("./genres.js");
+const User = users.model;
+const validAdmin = users.validAdmin;
+const Genre = users.model;
+
+//BOOK: Schema, Model, POST, GET for genre, UPDATE
+const bookSchema = new mongoose.Schema({
+    genre: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Genre'
+    },
+    name: String,
+    description: String,
+    photoPath: String,
+});
+
+//book model
+const Book = mongoose.model('Book', bookSchema);
+
+// Upload a photo. Uses the multer middleware for the upload and then returns
+// the path where the photo is stored in the file system.
+router.post('/photos', validAdmin, upload.single('photo'), async (req, res) => {
+    // Just a safety check
+    if (!req.file) {
+      return res.sendStatus(400);
+    }
+    res.send({
+      photoPath: "/images/" + req.file.filename
+    });
+  });
+
+//Create a Book w/ Genre
+router.post('/:genreID', validAdmin, async(req, res) => {
+    try {
+        let genre = await Genre.findOne({_id: req.params.genreID});
+        if (!genre) {
+            res.sendStatus(404);
+            return;
+        }
+        let book = new Book ({
+            genre: genre,
+            name: req.body.name,
+            description: req.body.description,
+            photoPath: req.body.photoPath,
+        });
+        await book.save();
+        res.send(book);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+//Get all the books in the database
+router.get('/', async(req, res) => {
+    try {
+        let books = await Book.find();
+        res.send(books);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+//Get all the books for a genre 
+router.get('/:genreID', async(req, res) => {
+    try {
+        let genre = await Genre.findOne({_id: req.params.genreID});
+        if (!genre) {
+            res.sendStatus(404);
+            return;
+        }
+        let books = await Book.find({genre:genre});
+        res.send(books);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+//Update a book
+router.put('/:bookID', validAdmin, async(req, res) => {
+    try {
+        let book = await Book.findOne({_id: req.params.bookID});
+        if(!book) {
+            res.sendStatus(404);
+            return;
+        }
+        book.name = req.body.name;
+        book.description = req.body.description;
+        //photoPath = req.body.photoPath;
+        await book.save();
+        res.send(book);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+//Delete a book
+router.delete('/:bookID', validAdmin, async(req, res) => {
+    try {
+        let book = await Book.findOne({_id: req.params.bookID});
+        if(!book) {
+            res.sendStatus(404);
+            return;
+        }
+        await book.delete();
+        res.sendStatus(200);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+module.exports = {
+    model: Book,
+    routes: router
+}
