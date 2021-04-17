128b11178aa16a6c846caab4158367fc993c7b0e
Hannah Nelson <hannahnelson@Hannahs-Air-010037171115.app.byu.edu>
2021-04-16 21:12:59 -0600

    front end infrastructure in place

---
+++ front-end/src/views/UserPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="userpage">
+    <h1>I am hannah</h1>  
+    <!-- <MyReviews v-if="user" />
+    <Login v-else />
+    <Admin v-if="user.role === 'admin'" /> -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserPage'
+}
+</script>
+
+
+<style scoped>
+
+.userpage {
+    color: black;
+}
+
+</style>
